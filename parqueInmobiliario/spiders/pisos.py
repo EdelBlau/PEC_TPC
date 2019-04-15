dbe7bdb87f557723d1dee6524ae2c30d68678dfd
EdelBlau <irenefm_3@hotmail.com>
2019-04-15 17:28:30 +0200

    parametro ciudad al ejecutar

--- parqueInmobiliario/spiders/pisos.py
+++ parqueInmobiliario/spiders/pisos.py
@@ -4,16 +4,17 @@ from parqueInmobiliario.items import InmuebleItem
 
 class PisosSpider(scrapy.Spider):
     name = 'pisos'
-    def start_requests(self):
-        urls = [
-            'https://www.pisos.com/venta/pisos-madrid/'
+
+    def __init__(self, *args, **kwargs):
+
+        super(PisosSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [
+            "https://www.pisos.com/venta/pisos-"+ kwargs.get('ciudad') +"/"
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for i in range(1,20):
-            link = "https://www.pisos.com/venta/pisos-madrid/" + str(i) + '/'
+            link = response.url + str(i) + '/'
             yield scrapy.Request(link, self.parse_pagina)
 
     def parse_pagina(self, response):
@@ -25,19 +26,15 @@ class PisosSpider(scrapy.Spider):
 
     def parse_vivienda(self, response):
         item = InmuebleItem()
-        item['precio'] = response.xpath( '//div[@class = "priceBox-price"]/span/text()' ).get()
+        item['precio'] = response.xpath( '//div[@class = "priceBox-price"]/span/text()' ).re_first(r'(.*)€')
         item['superficie'] = response.xpath( '//div[@class = "basicdata-item"]/text()' )[0].re_first(r'[0-9]+')
         item['habitaciones'] = response.xpath( '//div[@class = "basicdata-item"]/text()' )[1].re_first( r"[0-9]+" )
         item['banos'] = response.xpath( '//div[@class = "basicdata-item"]/text()' )[2].re_first( r'[0-9]+' )
         item['referencia'] = response.xpath('//li[@class="charblock-element more-padding"]//text()').re(r':(.*-.*)')
         item['particular'] = 'Profesional' if response.xpath( '//div[@class = "owner-data-logo"]' ) else 'Particular'
-        item['ciudad'] = response.xpath( '//h2[@class = "position"]/text()' ).re_first(r'\((.*)\)')
-        item['comunidad'] = response.xpath( '//h2[@class = "position"]/text()' ).extract_first()
-        
-        '''        
-        #item['codigoPostal'] = response.xpath( '//p[@class = "fc-DetailDescription"]/text()' ).re_first( r'[0-9]{5}' )
-        item['planta'] = response.xpath( '//li[@class = "re-DetailHeader-featuresItem"]/text()' )[3].re_first( r'[0-9]+' )
-        '''
+        item['ciudad'] = response.xpath( '//h2[@class = "position"]/text()' ).re_first(r'\((.*)\)') or response.xpath( '//h2[@class = "position"]/text()' ).extract_first()
+        item['comunidad'] = response.xpath( '//div[@class = "footer-breadcrumb"]/div[@class="item"][2]/a/text()' ).extract_first()
+    
         item['web'] = 'Pisos'
         item['link'] = response.url
 
